bc80b7c76c8c5411af65040d52d2fef7fc9e540e
Jane <73879019+Jane-UA@users.noreply.github.com>
2021-01-05 23:04:30 +0200

    task1.py

---
+++ task1.py
@@ -0,0 +1,7 @@
+"""create a python program named “task1” use the built-in function `print` in it several times'
+try to pass “sep”, “end” params and pass several parameters separated by commas"""
+print('Hello world!'), '# function print'
+print(1, 2, 3, 4, 5, 6, sep='_'), '# separate integers'
+print('hello', end='\n'), '#use end'
+print('my', end='\n')
+print('world!', end='\n')
---
+++ task2.py
@@ -0,0 +1,10 @@
+"""Write a program, which has two print statements
+ to print the following text
+ (capital letters “O” and “H”, made from “#” symbols)"""
+a = '#' * 9
+b = '#\t\t#'
+c = b+'\n'
+d = a+'\n'
+print(d,c,c,c,d)
+print(c,c,c,d,c,c,c)
+#не знаю почему появляется пробел перед строкой и не знаю как от него избавиться
